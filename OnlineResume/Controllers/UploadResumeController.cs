2b898f0f299e40a8cee3413b953bbb32ad8d34db
Scott Guyton (HCL America Inc) <v-scguyt@microsoft.com>
2019-04-04 12:37:29 -0700

    Added unit test project, added new controller for uploading files

--- OnlineResume/Controllers/UploadResumeController.cs
+++ OnlineResume/Controllers/UploadResumeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -11,5 +12,29 @@ namespace OnlineResume.Controllers
     [ApiController]
     public class UploadResumeController : ControllerBase
     {
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            // full path to file in temp location
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size, filePath });
+        }
     }
 }
